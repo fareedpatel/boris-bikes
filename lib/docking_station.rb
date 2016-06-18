92f73698b52e9e99ad9154ebeee0ddcd2d90385e
fareedpatel <fareed_patel@hotmail.co.uk>
2016-06-18 18:16:13 +0100

    initialization defaults

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,25 +1,31 @@
 require_relative 'bike'
-class DockingStation
-DEFAULT_CAPACITY = 20
 
-  def initialize
+class DockingStation
+ DEFAULT_CAPACITY = 20
+
+  attr_accessor :capacity
+
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @capacity = capacity
     @bikes = []
   end
+
+  def dock(bike)
+    fail 'Docking station full' if full?
+    @bikes << bike
+  end
+
   def release_bike
      fail "No bikes available" if empty?
      @bikes.pop
   end
-  def dock(bike)
-    # use an instance variable to store the bike
-    # in the 'state' of this insthttps://github.com/makersacademy/course/blob/master/boris_bikes/walkthroughs/15.mdance
-   	fail 'Docking station full' if full?
-    @bikes << bike
-  end
 
 private
 
+attr_reader :bikes
+
   def full?
-    @bikes.count >= DEFAULT_CAPACITY
+    @bikes.count >= capacity
   end
 
   def empty?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,19 @@
 require "docking_station"
 
 describe DockingStation  do
+  # it 'has a default capacity' do
+  #     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  #   end
+  describe 'initialization' do
+    subject { DockingStation.new }
+    let (:bike) { Bike.new }
+    it 'defaults capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.dock(bike)
+      end
+      expect{ subject.dock(bike) }.to raise_error 'Docking station full'
+    end
+  end
 	describe '#release_bike' do
     it 'releases a bike' do
       bike = Bike.new
@@ -9,11 +22,9 @@ describe DockingStation  do
       expect(subject.release_bike).to eq bike
     end
   end
-  describe '#dock' do
+  describe 'dock' do
     it 'raises an error when full' do
-      DockingStation::DEFAULT_CAPACITY.times do
-        subject.dock Bike.new
-      end
+        subject.capacity.times { subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
